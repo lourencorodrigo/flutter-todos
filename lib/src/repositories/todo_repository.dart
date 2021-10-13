23b7dd3786be0650822acb8dba2ad6e39645aaae
Rodrigo Lourenço <rsilvape@gmail.com>
2021-10-13 14:32:03 -0300

    48 - [API] Aprendendo a Refatorá o código

--- lib/src/repositories/todo_repository.dart
+++ lib/src/repositories/todo_repository.dart
@@ -6,23 +6,13 @@ class TodoRepository {
   final url = 'https://jsonplaceholder.typicode.com/todos';
 
   TodoRepository([Dio client]) {
-    if (client == null) {
-      this.dio = Dio();
-    } else {
-      this.dio = client;
-    }
+    dio = client ?? Dio();
   }
 
   Future<List<TodoModel>> fetchTodos() async {
     final response = await dio.get(url);
     final list = response.data as List;
 
-    List<TodoModel> todos = [];
-    for (var json in list) {
-      final todo = TodoModel.fromJson(json);
-      todos.add(todo);
-    }
-
-    return todos;
+    return list.map((json) => TodoModel.fromJson(json)).toList();
   }
 }
