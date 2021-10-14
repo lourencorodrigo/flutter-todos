546b067d73f46125a4a0c269c48f347391834265
Rodrigo Lourenço <rsilvape@gmail.com>
2021-10-13 21:21:57 -0300

    51 - [API] Gerenciamento de Estado

--- test/src/controllers/home_controller_test.dart
+++ test/src/controllers/home_controller_test.dart
@@ -8,12 +8,22 @@ class TodoRepositoryMock extends Mock implements TodoRepository {}
 
 main() {
   final repository = TodoRepositoryMock();
-  final controller = HomeController();
+  final controller = HomeController(repository);
 
   test('deve preencher variavel todos', () async {
     when(repository.fetchTodos()).thenAnswer((_) async => [TodoModel()]);
 
+    expect(controller.state, HomeState.start);
     await controller.start();
+    expect(controller.state, HomeState.success);
     expect(controller.todos.isNotEmpty, true);
+  });
+
+  test('deve modificar o estado para erro se a requisição falhar', () async {
+    when(repository.fetchTodos()).thenThrow(Exception());
+
+    expect(controller.state, HomeState.start);
+    await controller.start();
+    expect(controller.state, HomeState.error);
   });
 }
