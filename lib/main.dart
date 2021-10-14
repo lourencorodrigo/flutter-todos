57697e74d17c9db473357ecb7eb0919b3e37d3bf
Rodrigo Lourenço <rsilvape@gmail.com>
2021-10-13 21:30:48 -0300

    52 - [API] Criando os estados da View

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(primaryColor: Colors.red),
+      theme: ThemeData.dark(),
       home: HomePage(),
     );
   }
--- lib/src/home_page.dart
+++ lib/src/home_page.dart
@@ -1,7 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:todo/src/controllers/home_controller.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
+  _success() {
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return ListTile(title: Text('item $index'));
+      },
+    );
+  }
+
+  _error() {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text('TENTAR NOVAMENTE'),
+      ),
+    );
+  }
+
+  _start() {
+    return Container();
+  }
+
+  _loading() {
+    return Center(child: CircularProgressIndicator());
+  }
+
+  stateManagement(HomeState state) {
+    switch (state) {
+      case HomeState.start:
+        return _start();
+      case HomeState.loading:
+        return _loading();
+      case HomeState.error:
+        return _error();
+      case HomeState.success:
+        return _success();
+      default:
+        return _start();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,12 +49,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('List Todo\'s'),
       ),
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return ListTile(title: Text('item $index'));
-        },
-      ),
+      body: stateManagement(HomeState.success),
     );
   }
 }
